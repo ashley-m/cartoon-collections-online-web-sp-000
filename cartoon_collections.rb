14a9833a330ded19a2c3623c32e68fa4dbde6a0e
Ashley Miller <asm2fe@virginia.edu>
2019-03-31 19:15:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,8 +9,9 @@ def summon_captain_planet(collection)# code an argument here
   return collection.collect {|lmnt| lmnt.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(collection)# code an argument here
   # Your code here
+  return collection.all? {|long| long.length > 4}
 end
 
 def find_the_cheese# code an argument here
